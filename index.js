68f108bffbd9f1a0eb60439e16334970154c891d
Nafie Asfour <ph.nafee.asfour@gmail.com>
2024-06-14 23:24:20 +0300

    FirstStepinTypeScript

---
+++ index.js
@@ -0,0 +1,8 @@
+// Define a function to greet a user
+function greet(name) {
+    return "Hello, ".concat(name, "!");
+}
+// Call the function with a sample name
+var greetingMessage = greet('Alice');
+// Print the greeting message to the console
+console.log(greetingMessage);
---
+++ index.ts
@@ -0,0 +1,10 @@
+// Define a function to greet a user
+function greet(name: string): string {
+    return `Hello, ${name}!`;
+}
+
+// Call the function with a sample name
+const greetingMessage = greet('Alice');
+
+// Print the greeting message to the console
+console.log(greetingMessage);
